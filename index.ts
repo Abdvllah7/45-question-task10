9515bc913e07853b62c352b3cfe506c4b81f4fc6
Abdvllah7 <czczcsxvdg@gmail.com>
2024-05-09 06:16:53 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+//This video is for comment tutorial
+
+ //please dont change line 3 to 6. (abdullah)
+let PersonName: string = "muhammad abdullah";
+
+// step 1 show in lower case:
+
+let lowercaseName: string = PersonName.toLowerCase();
+
+console.log(lowercaseName);
+
+//step 2 show name in uppercase:
+
+let uppercaseName: string = PersonName.toUpperCase();
+
+console.log(uppercaseName);
+
+// step 3 show name in titlecase
+
+let words: string[] = PersonName.split(" ");
+
+let titlecaseName: string = ""
+
+for (let i = 0 ; i < words.length; i++) {
+
+    titlecaseName +=words[i].charAt(0).toUpperCase() +words[i].slice(1).toLowerCase()+" "
+};
+
+console.log(titlecaseName)
